77460e6c8752c6da0ac787146c32d31b28ccd9cb
Jan Švábík <jansvabik@jansvabik.cz>
2021-01-02 19:36:58 +0100

    feat: add lock and unlock handlers

--- cmd/websocket.go
+++ cmd/websocket.go
@@ -2,8 +2,8 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"log"
+	"time"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/websocket/v2"
@@ -30,10 +30,101 @@ type ResponseMessage struct {
 }
 
 // connections is a map which contains all websocket connections
-var connections map[int]*websocket.Conn = make(map[int]*websocket.Conn)
+var connections = make(map[int]*websocket.Conn)
 
 // counter is a counter for new connection ids
-var counter int = 0
+var counter = 0
+
+// lockedSeats is a array of locked seat identifiers
+var lockedSeats = make(map[string]time.Time)
+
+// broadcastMessage sends a message to all clients, omitting
+// the one which is specified in an omit param
+func broadcastMessage(res ResponseMessage, omit int) {
+	// send the change (event) to all connected clients
+	for clientID, client := range connections {
+		// omit current connection
+		if clientID == omit {
+			continue
+		}
+
+		// create the message string value from structure
+		dStr, err := json.Marshal(res)
+		if err != nil {
+			log.Println("Error:", err.Error())
+			continue
+		}
+
+		// send the message
+		err = client.WriteMessage(1, dStr)
+		if err != nil {
+			log.Println("Error:", err.Error())
+			continue
+		}
+	}
+}
+
+// handleLock locks the specified seat for lockerID and sends a message
+// to all connected clients about the new locked seat (on success)
+func handleLock(c *websocket.Conn, sr SeatRequest, lockerID int) {
+	if _, ok := lockedSeats[sr.Seat]; ok {
+		dStr, _ := json.Marshal(ResponseMessage{
+			Event: "alreadyLocked",
+			Data:  sr.Seat,
+		})
+		c.WriteMessage(1, dStr)
+		return
+	}
+
+	// lock the seat for the specified amount of time
+	lockedSeats[sr.Seat] = time.Now().Add(120 * time.Second)
+
+	// create the message
+	dStr, _ := json.Marshal(ResponseMessage{
+		Event: "lockedForYou",
+		Data:  sr.Seat,
+	})
+
+	// send messages back to the client
+	c.WriteMessage(1, dStr)
+
+	// send locked message to all clients
+	broadcastMessage(ResponseMessage{
+		Event: "locked",
+		Data:  sr.Seat,
+	}, lockerID)
+}
+
+// handleUnlock unlocks the specified seat and sends a message to all
+// connected clients about the new unlocked seat (on success)
+func handleUnlock(c *websocket.Conn, sr SeatRequest, lockerID int) {
+	if _, ok := lockedSeats[sr.Seat]; !ok {
+		dStr, _ := json.Marshal(ResponseMessage{
+			Event: "alreadyUnlocked",
+			Data:  sr.Seat,
+		})
+		c.WriteMessage(1, dStr)
+		return
+	}
+
+	// unlock the seat
+	delete(lockedSeats, sr.Seat)
+
+	// create the message
+	dStr, _ := json.Marshal(ResponseMessage{
+		Event: "unlockedForYou",
+		Data:  sr.Seat,
+	})
+
+	// send messages back to the client
+	c.WriteMessage(1, dStr)
+
+	// send locked message to all clients
+	broadcastMessage(ResponseMessage{
+		Event: "unlocked",
+		Data:  sr.Seat,
+	}, lockerID)
+}
 
 func main() {
 	app := fiber.New()
@@ -60,13 +151,19 @@ func main() {
 			err error
 		)
 
+		// create a slice of strings from locked seats
+		lockedStringList := []string{}
+		for k := range lockedSeats {
+			lockedStringList = append(lockedStringList, k)
+		}
+
 		// get current data
 		cd := ResponseMessage{
 			Event: "startstate",
 			Data: SeatsState{
 				Reserved: []string{"A_2_2", "A_2_3", "A_2_1", "A_2_4", "V_60", "V_61"},
 				Paid:     []string{"V_1", "V_2", "V_3", "V_4", "G_2_1", "G_2_2", "A_18_1", "A_18_2", "A_18_3", "A_18_4"},
-				Locked:   []string{"A_10_1", "A_10_2"},
+				Locked:   lockedStringList,
 			},
 		}
 
@@ -97,48 +194,11 @@ func main() {
 			}
 
 			// valid json, do the job
-			fmt.Println(sr.Action)
-			fmt.Println(sr.Seat)
-
-			// send the change (event) to all connected clients
-			for clientID, client := range connections {
-				// omit current connection
-				if clientID == thisID {
-					continue
-				}
-
-				// create the message string value from structure
-				dStr, err := json.Marshal(ResponseMessage{
-					Event: sr.Action + "ed",
-					Data:  sr.Seat,
-				})
-				if err != nil {
-					log.Println("Error:", err.Error())
-					continue
-				}
-
-				// send the message
-				err = client.WriteMessage(1, dStr)
-				if err != nil {
-					log.Println("Error:", err.Error())
-					continue
-				}
-			}
-
-			// create the message
-			dStr, err := json.Marshal(ResponseMessage{
-				Event: sr.Action + "edForYou",
-				Data:  sr.Seat,
-			})
-			if err != nil {
-				log.Println("Error:", err.Error())
-				continue
-			}
-
-			// send messages back to the client
-			if err = c.WriteMessage(mt, dStr); err != nil {
-				log.Println("Error while sending message:", err.Error())
-				break
+			switch sr.Action {
+			case "lock":
+				handleLock(c, sr, thisID)
+			case "unlock":
+				handleUnlock(c, sr, thisID)
 			}
 		}
 	}))
