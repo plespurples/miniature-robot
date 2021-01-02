c876717fd2ad025f68937ad4b14dfaeeac77224f
Jan Švábík <jansvabik@jansvabik.cz>
2021-01-02 16:10:51 +0100

    feat: add testing data

--- cmd/websocket.go
+++ cmd/websocket.go
@@ -16,6 +16,12 @@ type SeatRequest struct {
 	Seat   string `json:"seat"`
 }
 
+type SeatsState struct {
+	Reserved []string `json:"reserved"`
+	Busy     []string `json:"busy"`
+	Clicked  []string `json:"clicked"`
+}
+
 func main() {
 	app := fiber.New()
 
@@ -30,15 +36,29 @@ func main() {
 
 	// websocket endpoint
 	app.Get("/", websocket.New(func(c *websocket.Conn) {
-		// websocket.Conn bindings https://pkg.go.dev/github.com/fasthttp/websocket?tab=doc#pkg-index
 		var (
 			mt  int
 			msg []byte
 			err error
 		)
 
+		// get current data
+		cd := SeatsState{
+			Reserved: []string{"A_2_2", "A_2_3", "A_2_1", "A_2_4", "V_60", "V_61"},
+			Busy:     []string{"V_1", "V_2", "V_3", "V_4", "G_2_1", "G_2_2", "A_18_1", "A_18_2", "A_18_3", "A_18_4"},
+			Clicked:  []string{"A_10_1", "A_10_2"},
+		}
+
+		// marshal the data to json string
+		currentStateString, err := json.Marshal(cd)
+		if err != nil {
+			log.Println("Cannot marshal the current data", cd)
+		}
+
 		// this happen when client is connected to the server
-		fmt.Println("New connection established")
+		if err = c.WriteMessage(1, currentStateString); err != nil {
+			log.Println("Error while sending message:", err)
+		}
 
 		// this will happen on every message/connection
 		for {
